d6edde744204bdeb4b71a4f48936d3c6c890238e
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-10-25 11:44:26 +0100

    Modify error handler response message

--- server/app.js
+++ server/app.js
@@ -22,15 +22,9 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
-  console.log('using this error handler');
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.send({
-    error: 'Not found',
+    error: `${req.url} Not found`,
     message: 'specified path Not Found',
   });
 });
