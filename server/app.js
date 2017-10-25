792c85aa55360cc5f484ca122f7402a71b019809
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-10-25 09:27:09 +0100

    Initial project backend/api set up

---
+++ server/app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
+import index from './routes/index';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', index);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  console.log('using this error handler');
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send({
+    error: 'Not found',
+    message: 'specified path Not Found',
+  });
+});
+
+export default app;
