98938ed6735b3f84685015d224ebb0fbc1b6c0ce
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-10-27 12:44:02 +0100

    Modify default message for root API route

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,6 +5,12 @@ import RecipeController from '../controllers/recipe';
 const router = express.Router();
 const recipeRouter = express.Router();
 
+router.get('/', (req, res) => {
+  res.json({
+    message: 'More-Recipes API service',
+  });
+});
+
 const Recipe = new RecipeController(recipeRouter);
 router.use('/recipes', recipeRouter);
 
