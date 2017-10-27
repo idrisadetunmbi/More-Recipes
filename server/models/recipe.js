9defd909fac823856bf180f3f068cd54067efefa
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-10-27 12:42:45 +0100

    Add more properties to models

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -1,12 +1,15 @@
+import uuid from 'uuid';
+
 export default class Recipe {
   constructor(reqBody) {
+    this.id = uuid.v4();
     this.title = reqBody.title;
     this.description = reqBody.description;
     this.ingredients = reqBody.ingredients;
     this.directions = reqBody.directions;
     this.upvotes = 0;
     this.downvotes = 0;
-    this.downvotes = 0;
+    this.favorites = 0;
     this.category = reqBody.category || '';
   }
 }
