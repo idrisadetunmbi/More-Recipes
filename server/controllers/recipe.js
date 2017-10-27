f0be94550b386549cd4ee47f99a07c1a006c1eae
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-10-27 12:41:04 +0100

    Implement more routes controller and modify existing ones

--- server/controllers/recipe.js
+++ server/controllers/recipe.js
@@ -1,5 +1,6 @@
-import { check, validationResult } from 'express-validator/check';
+import validate, { validationResult } from 'express-validator/check';
 import RecipesService from '../services/RecipesService';
+import ReviewService from '../services/ReviewService';
 
 export default class RecipeController {
   constructor(router) {
@@ -10,47 +11,64 @@ export default class RecipeController {
   /**
    * Array storing validations for a POST recipe request
    */
-  postRecipeValidationArray = [
-    check('title', 'title must be between 5 to 50 characters').isLength({ min: 5, max: 50 }).trim(),
-    check('description', 'you must add a description').exists(),
-    check('ingredients', 'ingredients must be included').exists(),
-    check('directions', 'directions are required for a recipe').exists(),
+  recipePostValidationChecks = [
+    validate.body('title', 'title must be between 5 to 50 characters')
+      .isLength({ min: 5, max: 50 }).not().isInt()
+      .trim(),
+    validate.body('description', 'you must add a description').exists(),
+    validate.body('ingredients', 'ingredients must be included').exists(),
+    validate.body('directions', 'directions are required for a recipe').exists(),
   ];
 
   /**
    * Stores validation for a GET request
    */
-  getRecipeValidationArray = [
-    check('id', 'recipe id is not included or is invalid').isUUID(),
+  recipeGetValidationChecks = [
+    validate.param('id', 'recipe id is invalid').isUUID(),
+  ];
+
+  recipePutValidationChecks = [
+    validate.body('title', 'title must be between 5 to 50 characters')
+      .isLength({ min: 5, max: 50 }).not().isInt()
+      .optional()
+      .trim(),
+  ];
+
+  reviewPostValidationChecks = [
+    validate.body('review', 'you must add a review with at least 5 characters').isLength({ min: 5 }),
+    validate.param('recipeId', 'invalid recipe id or recipe does not exist').isUUID()
+      .custom((value, { req }) => RecipesService.getRecipe(value)),
   ];
 
   registerRoutes() {
-    this.router.get('/:id', this.getRecipeValidationArray, this.getRecipe);
     this.router.get('/', this.getRecipes);
-    this.router.post('/', this.postRecipeValidationArray, this.validatePostRecipeData, this.postRecipe);
-    this.router.put('/:id', this.validatePutRecipeData, this.putRecipe);
-    this.router.delete('/:id', this.deleteRecipe);
-  }
-
-  getRecipe = (req, res) => {
-    try {
-      validationResult(req).throw();
-    } catch (error) {
-      return res.status(422).json({
-        error: error.array()[0],
-      });
-    }
-    const recipe = RecipesService.getRecipe(req.params.id);
-    return res.status(200).send({
-      data: recipe,
-    });
+    this.router.get('/:id', this.recipeGetValidationChecks, this.validateRequestData, this.getRecipe);
+    this.router.post('/', this.recipePostValidationChecks, this.validateRequestData, this.postRecipe);
+    this.router.put('/:id', [...this.recipeGetValidationChecks, ...this.recipePutValidationChecks], this.validateRequestData, this.putRecipe);
+    this.router.delete('/:id', this.recipeGetValidationChecks, this.validateRequestData, this.deleteRecipe);
+    this.router.post('/:recipeId/reviews', this.reviewPostValidationChecks, this.validateRequestData, this.postRecipeReview);
+    this.router.get('/:recipeId/reviews', [this.reviewPostValidationChecks[1]], this.validateRequestData, this.getRecipeReviews);
   }
 
   getRecipes = (req, res) => {
     const recipes = RecipesService.getRecipes();
+    if (req.query.sort) {
+      recipes.sort((upvotesA, upvotesB) => upvotesB - upvotesA);
+    }
     res.status(200).send({
       data: recipes,
     });
+  }
+
+  getRecipe = (req, res) => {
+    const recipe = RecipesService.getRecipe(req.params.id);
+    return recipe ?
+      res.status(200).send({
+        data: recipe,
+      }) :
+      res.status(404).send({
+        error: `recipe with id - ${req.params.id} does not exist`,
+      });
   }
 
   /**
@@ -59,38 +77,70 @@ export default class RecipeController {
    */
   postRecipe = (req, res) => {
     const recipe = RecipesService.addRecipe(req.body);
-    if (!recipe) {
+    return recipe ?
+      res.status(201).send({
+        message: 'Recipe created successfully',
+        data: { ...req.body, id: recipe },
+      }) :
       res.status(400).send({
         message: 'Recipe with title already exists, please choose another title',
       });
-      return;
-    }
-    res.status(201).send({
-      message: 'Recipe created successfully',
-      data: { ...req.body, id: recipe },
-    });
-  }
-
-  validatePostRecipeData = (req, res, next) => {
-    const results = validationResult(req);
-    if (!results.isEmpty()) {
-      return res.status(422).json({
-        message: 'one or more of the required request data is not included or is invalid',
-        errors: results.array(),
-      });
-    }
-    return next();
-  }
-
-  validatePutRecipeData = (req, res, next) => {
-
   }
 
   putRecipe = (req, res) => {
-
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).send({
+        error: 'request body does not contain any data',
+      });
+    }
+    const response = RecipesService.modifyRecipe(req.params.id, req.body);
+    return response ?
+      res.status(200).send({
+        message: `recipe with id - ${req.params.id} has been successfully deleted`,
+      }) :
+      res.status(404).send({
+        error: 'recipe with the specified id does not exist',
+      });
   }
 
   deleteRecipe = (req, res) => {
+    const result = RecipesService.deleteRecipe(req.params.id);
+    return result ?
+      res.status(200).send({
+        message: `recipe with id - ${req.params.id} has been successfully deleted`,
+      }) :
+      res.status(404).send({
+        error: `recipe with id - ${req.params.id} does not exist`,
+      });
+  }
 
+  postRecipeReview = (req, res) => {
+    return ReviewService.addReview({ ...req.body, recipeId: req.params.recipeId }) ?
+      res.status(201).json({
+        message: 'review added successfully',
+      }) :
+      res.status(422).json({
+        error: 'request data with the same details already exist',
+      });
+  }
+
+  getRecipeReviews = (req, res) => {
+    const recipeReviews = ReviewService.getReviews(req.params.recipeId);
+    return recipeReviews ?
+      res.status(200).json({
+        data: recipeReviews,
+      }) :
+      res.status(204).send();
+  }
+
+  validateRequestData = (req, res, next) => {
+    const results = validationResult(req);
+    return results.isEmpty() ?
+      next() :
+      res.status(422).json({
+        message: 'one or more of the required request data is not included or is invalid',
+        errors: results.array(),
+      });
   }
 }
+
