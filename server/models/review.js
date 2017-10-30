2500510615889a1d116a1a779bf779711a19145b
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-10-30 11:37:16 +0100

    Remove title property from review model

--- server/models/review.js
+++ server/models/review.js
@@ -3,7 +3,6 @@ import uuid from 'uuid';
 export default class RecipeReview {
   constructor(reviewData) {
     this.id = uuid.v4();
-    this.title = reviewData.title || '';
     this.review = reviewData.review;
     this.postedBy = reviewData.user || 'anonymous user';
     this.recipeId = reviewData.recipeId;
