ce2a347ff87f07e327ce461b1b00b326f4ee365e
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-11-01 03:34:57 +0100

    Modify routes url parameters

--- server/routes/recipe_routes.js
+++ server/routes/recipe_routes.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import RecipeController from '../controllers/recipe';
+import RecipeController from '../controllers/recipe_controller';
 
 const router = express.Router();
 const controller = new RecipeController();
 
 router.get('/', controller.getRecipes);
-router.get('/:id', controller.recipeGetValidationChecks, controller.validateRequestData, controller.getRecipe);
+router.get('/:recipeId', controller.recipeGetValidationChecks, controller.validateRequestData, controller.getRecipe);
 router.post('/', controller.recipePostValidationChecks, controller.validateRequestData, controller.postRecipe);
-router.put('/:id', [...controller.recipeGetValidationChecks, ...controller.recipePutValidationChecks], controller.validateRequestData, controller.putRecipe);
-router.delete('/:id', controller.recipeGetValidationChecks, controller.validateRequestData, controller.deleteRecipe);
+router.put('/:recipeId', [...controller.recipeGetValidationChecks, ...controller.recipePutValidationChecks], controller.validateRequestData, controller.putRecipe);
+router.delete('/:recipeId', controller.recipeGetValidationChecks, controller.validateRequestData, controller.deleteRecipe);
 router.post('/:recipeId/reviews', controller.reviewPostValidationChecks, controller.validateRequestData, controller.postRecipeReview);
 router.get('/:recipeId/reviews', [controller.reviewPostValidationChecks[1]], controller.validateRequestData, controller.getRecipeReviews);
 
