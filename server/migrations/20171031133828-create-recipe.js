3daed521b6e92fc6c6291f7623fe74e011cb7387
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-11-01 03:36:47 +0100

    Add recipe model for sequelize

---
+++ server/migrations/20171031133828-create-recipe.js
@@ -0,0 +1,49 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recipes', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      creatorId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+          as: 'creatorId',
+        },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      ingredients: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.STRING,
+        defaultValue: 'uncategorized',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('recipes');
+  },
+};
---
+++ server/models/sequelize_models/recipe.js
@@ -0,0 +1,41 @@
+export default (sequelize, DataTypes) => {
+  const Recipe = sequelize.define('recipe', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    // creatorId: {
+    //   type: DataTypes.UUID,
+    //   allowNull: false,
+    // },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ingredients: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    category: {
+      type: DataTypes.STRING,
+      defaultValue: 'uncategorized',
+    },
+  }, {
+    classMethods: {
+      associate: (models) => {
+        // associations can be defined here
+      },
+    },
+  });
+  Recipe.associate = (models) => {
+    Recipe.belongsTo(models.user, { as: 'creator', foreignKey: 'creatorId' });
+    Recipe.belongsToMany(models.user, { as: 'userActions', through: models.recipe_action });
+  };
+  return Recipe;
+};
+
