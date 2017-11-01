96df694263792610bde4502531dc985ef01cdb1f
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-11-01 11:24:59 +0100

    Allow null to be added for vote property

--- server/models/sequelize_models/recipe_action.js
+++ server/models/sequelize_models/recipe_action.js
@@ -33,6 +33,7 @@ export default (sequelize, DataTypes) => {
     vote: {
       type: DataTypes.ENUM,
       values: ['upvote', 'downvote'],
+      allowNull: true,
     },
   }, {
     classMethods: {
