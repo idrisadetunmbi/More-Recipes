29490fb8c0de27b33bfa93725258f6f0b6001565
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-11-02 16:48:46 +0100

    Remove unnecessary comment

--- server/models/index.js
+++ server/models/index.js
@@ -7,7 +7,6 @@ import configFile from '../config/config';
 dotenv.config({ path: `${__dirname}/../../.env` });
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.js')[env];
 const config = configFile[env];
 const db = {};
 
