e9fcbe576433ac13a503365b9ae89363bf5c0982
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-11-02 06:14:56 +0100

    Improve readabiltiy and add more endpoints

--- server/routes/user_routes.js
+++ server/routes/user_routes.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import UserController from '../controllers/user_controller';
+import authenticateUser from '../middlewares/authentication';
 
 const router = express.Router();
 const userController = new UserController();
+
+router.get('/', userController.getAllUsers);
 
 router.post(
   '/signup', userController.userSignUpValidationChecks,
@@ -12,6 +15,11 @@ router.post(
 router.post(
   '/signin', userController.userSignUpValidationChecks.slice(0, 2),
   userController.validateRequestData, userController.signInUser,
+);
+
+router.get(
+  '/:userId/recipes', authenticateUser, userController.userGetRecipesValidationChecks,
+  userController.validateRequestData, userController.getUserFavoriteRecipes,
 );
 
 
