c4f98f8ed85e296890cf00e0ddbff62dca5a1cfa
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-11-02 16:28:05 +0100

    Update config.json

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,5 @@
 require('dotenv').config({ path: `${__dirname}/../../.env` });
 
-
 module.exports = {
   development: {
     use_env_variable: 'DEV_DATABASE_URL',
@@ -14,22 +13,4 @@ module.exports = {
     use_env_variable: 'TEST_DATABASE_URL',
     dialect: 'postgres',
   },
-  // development: {
-  //   use_env
-  //   dialect: 'mysql',
-  // },
-  // test: {
-  //   username: 'root',
-  //   password: null,
-  //   database: 'database_test',
-  //   host: '127.0.0.1',
-  //   dialect: 'mysql',
-  // },
-  // production: {
-  //   username: 'root',
-  //   password: null,
-  //   database: 'database_production',
-  //   host: '127.0.0.1',
-  //   dialect: 'mysql',
-  // },
 };
