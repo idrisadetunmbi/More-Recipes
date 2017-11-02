1bb6b62f2fc739b1556e5fffa82d822a7f253b93
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-11-02 16:48:01 +0100

    Introduce url versioning

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,16 +1,9 @@
 import express from 'express';
-import recipeRouter from './recipe_routes';
-import userRouter from './user_routes';
+import v1Router from './v1';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'More-Recipes API service',
-  });
-});
-
-router.use('/users', userRouter);
-router.use('/recipes', recipeRouter);
+router.use('/', v1Router);
+router.use('/v1', v1Router);
 
 export default router;
