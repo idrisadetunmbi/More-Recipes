e2e8507ad5a98f47c266aa9444acdc9e9f2b108a
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-11-02 06:12:44 +0100

    Improve readability and use authentication middleware

--- server/routes/recipe_routes.js
+++ server/routes/recipe_routes.js
@@ -1,15 +1,45 @@
 import express from 'express';
 import RecipeController from '../controllers/recipe_controller';
+import authenticateUser from '../middlewares/authentication';
 
 const router = express.Router();
 const controller = new RecipeController();
 
-router.get('/', controller.getRecipes);
-router.get('/:recipeId', controller.recipeGetValidationChecks, controller.validateRequestData, controller.getRecipe);
-router.post('/', controller.recipePostValidationChecks, controller.validateRequestData, controller.postRecipe);
-router.put('/:recipeId', [...controller.recipeGetValidationChecks, ...controller.recipePutValidationChecks], controller.validateRequestData, controller.putRecipe);
-router.delete('/:recipeId', controller.recipeGetValidationChecks, controller.validateRequestData, controller.deleteRecipe);
-router.post('/:recipeId/reviews', controller.reviewPostValidationChecks, controller.validateRequestData, controller.postRecipeReview);
-router.get('/:recipeId/reviews', controller.recipeGetValidationChecks, controller.validateRequestData, controller.getRecipeReviews);
+router.get('/', authenticateUser, controller.getAllRecipes);
+
+router.get(
+  '/:recipeId', authenticateUser, controller.recipeGetValidationChecks,
+  controller.validateRequestData, controller.getRecipe,
+);
+
+router.post(
+  '/', authenticateUser, controller.recipePostValidationChecks,
+  controller.validateRequestData, controller.createRecipe,
+);
+
+router.put(
+  '/:recipeId', authenticateUser, [...controller.recipeGetValidationChecks, ...controller.recipePutValidationChecks],
+  controller.validateRequestData, controller.modifyRecipe,
+);
+
+router.delete(
+  '/:recipeId', authenticateUser, controller.recipeGetValidationChecks,
+  controller.validateRequestData, controller.deleteRecipe,
+);
+
+router.post(
+  '/:recipeId', authenticateUser, [...controller.recipeGetValidationChecks, ...controller.voteRecipeValidationCheck],
+  controller.validateRequestData, controller.voteRecipe,
+);
+
+router.post(
+  '/:recipeId/reviews', authenticateUser, controller.reviewPostValidationChecks,
+  controller.validateRequestData, controller.postRecipeReview,
+);
+
+router.get(
+  '/:recipeId/reviews', authenticateUser, controller.recipeGetValidationChecks,
+  controller.validateRequestData, controller.getRecipeReviews,
+);
 
 export default router;
