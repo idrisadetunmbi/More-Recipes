ecd2491638f7abab9c8a561f6382f21bbacfe192
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-11-12 00:18:13 +0100

    Verfiy token owner still exists in database in case of a deletion

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -1,13 +1,16 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import models from '../models';
 
 dotenv.config({ path: `${__dirname}/../../.env` });
 
-export default (req, res, next) => {
+const UserModel = models.user;
+
+export default async (req, res, next) => {
   let token = req.headers.authorization;
   if (!token) {
     return res.status(401).send({
-      error: 'you are not signed in or invalid token',
+      error: 'authentication is required for this action, please include user token',
     });
   }
   token = req.headers.authorization.slice(7);
@@ -16,7 +19,13 @@ export default (req, res, next) => {
     decodedPayload = jwt.verify(token, process.env.JWT_AUTH_SECRET);
   } catch (error) {
     return res.status(401).send({
-      error: 'you are not signed in or invalid token',
+      error: 'invalid or expired token',
+    });
+  }
+  const user = await UserModel.findById(decodedPayload.id);
+  if (!user) {
+    return res.status(404).send({
+      message: 'user does not exist, please sign up a new account',
     });
   }
   req.user = decodedPayload;
