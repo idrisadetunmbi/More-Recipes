f6690ffb829b0b6fe64d0db9540c9c45d709c91a
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-11-12 00:20:39 +0100

    Separate voting actions into different tables

---
+++ server/migrations/20171031140732-create-favorite.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('favorites', {
+      userId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        allowNull: false,
+      },
+      recipeId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'recipes',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('favorites');
+  },
+};
