1aafd46dba035d5c9f27d6729be2984706ca5bf8
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-11-15 09:28:49 +0100

    Integrate API documentation

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+
+import apiDocs from '../api-docs.json';
 import v1Router from './v1';
 
 const router = express.Router();
+router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(apiDocs));
 
 router.use('/', v1Router);
 router.use('/v1', v1Router);
--- server/routes/recipe_routes.js
+++ server/routes/recipe_routes.js
@@ -5,40 +5,42 @@ import authenticateUser from '../middlewares/authentication';
 const router = express.Router();
 const controller = new RecipeController();
 
-router.get('/', authenticateUser, controller.getAllRecipes);
+router.use(authenticateUser);
+
+router.get('/', controller.getAllRecipes);
 
 router.get(
-  '/:recipeId', authenticateUser, controller.recipeGetValidationChecks,
+  '/:recipeId', controller.recipeGetValidationChecks,
   controller.validateRequestData, controller.getRecipe,
 );
 
 router.post(
-  '/', authenticateUser, controller.recipePostValidationChecks,
+  '/', controller.recipePostValidationChecks,
   controller.validateRequestData, controller.createRecipe,
 );
 
 router.put(
-  '/:recipeId', authenticateUser, [...controller.recipeGetValidationChecks, ...controller.recipePutValidationChecks],
+  '/:recipeId', [...controller.recipeGetValidationChecks, ...controller.recipePutValidationChecks],
   controller.validateRequestData, controller.modifyRecipe,
 );
 
 router.delete(
-  '/:recipeId', authenticateUser, controller.recipeGetValidationChecks,
+  '/:recipeId', controller.recipeGetValidationChecks,
   controller.validateRequestData, controller.deleteRecipe,
 );
 
 router.post(
-  '/:recipeId', authenticateUser, [...controller.recipeGetValidationChecks, ...controller.voteRecipeValidationCheck],
+  '/:recipeId', [...controller.recipeGetValidationChecks, ...controller.voteRecipeValidationCheck],
   controller.validateRequestData, controller.voteRecipe,
 );
 
 router.post(
-  '/:recipeId/reviews', authenticateUser, [...controller.recipeGetValidationChecks, ...controller.reviewPostValidationChecks],
+  '/:recipeId/reviews', [...controller.recipeGetValidationChecks, ...controller.reviewPostValidationChecks],
   controller.validateRequestData, controller.postRecipeReview,
 );
 
 router.get(
-  '/:recipeId/reviews', authenticateUser, controller.recipeGetValidationChecks,
+  '/:recipeId/reviews', controller.recipeGetValidationChecks,
   controller.validateRequestData, controller.getRecipeReviews,
 );
 
