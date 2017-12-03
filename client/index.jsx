3368529edfda457cef973af76961be40bde3741d
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-12-03 22:33:13 +0100

    Remove unnecessary modules

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import Root from './components/containers/Root';
 import store from './store';
 
