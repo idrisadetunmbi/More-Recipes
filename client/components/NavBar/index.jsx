2958a6a4a830aef02cfabe8e0af39057c3a05adb
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-12-04 06:29:14 +0100

    Modify nav bar links according to user authentication status

--- client/components/NavBar/index.jsx
+++ client/components/NavBar/index.jsx
@@ -1,36 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './index.css';
 import Logo from './logo.jpg';
 
-const NavBar = props => (
-  <nav className="white">
-    <ul className="nav-wrapper container">
-      <div id="logo-container">
-        <a href="." className="brand-logo">
-          <img style={{ height: '56px', verticalAlign: 'middle' }} src={Logo} alt="" />
-        </a>
-      </div>
-      <ul className="right">
-        <li>
-          <Link to={{pathname: '/signin', state: { modal: true, previousLocation: props.location.pathname }}} className="dropdown-button" data-activates="dropdown">
-            <i style={{ fontSize: '3rem' }} className="large material-icons">
-              account_circle
-              <i style={{ marginLeft: '0px' }} className="material-icons right">arrow_drop_down</i>
-            </i>
-          </Link>
-        </li>
-      </ul>
-      
-      <ul id="dropdown" className="dropdown-content">
-        <li>
-          <Link to={{pathname: '/signin', state: { modal: true, previousLocation: props.location.pathname }}}>Sign In</Link>
-        </li>
-      </ul>
+const NavBar = (props) => {
+  const linkTo = props.user.data.token ?
+    '/user' :
+    {
+      pathname: '/signin',
+      state: {
+        modal: true,
+        previousLocation: props.location.pathname,
+      },
+    };
 
-    </ul>
-  </nav>
-);
 
-export default NavBar;
+  return (
+    <nav className="white">
+      <ul className="nav-wrapper container">
+        <div id="logo-container">
+          <a href="." className="brand-logo">
+            <img style={{ height: '56px', verticalAlign: 'middle' }} src={Logo} alt="" />
+          </a>
+        </div>
+        <ul className="right">
+          <li>
+            <Link to={linkTo} className="dropdown-button" data-activates="dropdown">
+              <i style={{ fontSize: '3rem' }} className="large material-icons">
+                account_circle
+                <i style={{ marginLeft: '0px' }} className="material-icons right">arrow_drop_down</i>
+              </i>
+            </Link>
+          </li>
+        </ul>
+        
+        <ul id="dropdown" className="dropdown-content">
+          <li>
+            { props.user.data.token &&
+              <Link to={{pathname: '/signin', state: { modal: true, previousLocation: props.location.pathname }}}>Sign In</Link>
+            }
+          </li>
+        </ul>
+
+      </ul>
+    </nav>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps)(NavBar);
